e221c3c219ca4edeed1fc90be18783717e776d11
JovannyRch <43225215+JovannyRch@users.noreply.github.com>
2020-06-05 08:37:56 -0500

    Home Screen

---
+++ app/src/main/java/com/jovannyrch/jovannyconf/ui/activities/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.jovannyrch.jovannyconf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import  com.jovannyrch.jovannyconf.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
